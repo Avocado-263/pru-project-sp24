32985e56ba626ab325e4606f6de58f9351e009fe
Thành <thanh17042001@gmail.com>
2024-01-17 00:37:40 +0700

    Animation Player Run

--- project_pru/Assets/Scripts/GamePlay/Player/Controller.cs
+++ project_pru/Assets/Scripts/GamePlay/Player/Controller.cs
@@ -5,36 +5,77 @@ using UnityEngine;
 public class Controller : MonoBehaviour
 {
     [SerializeField]
-    private float speed;
+    private float speed = 10;
     [SerializeField]
-    private float jumpForce;
+    private float jumpForce = 10;
     private bool isGrounded;
     private Rigidbody2D rb;
+    [SerializeField]
+    private Animator animator;
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         Move();
         Jump();
+        Rolling();
     }
 
     void Move()
     {
         float move = Input.GetAxis("Horizontal");
         Vector2 movement = new Vector2(move, 0);
-        rb.velocity = new Vector2(movement.x * speed, rb.velocity.y);
-    }
+        if (isGrounded)
+        {
+            rb.velocity = new Vector2(movement.x * speed, rb.velocity.y);
+            animator.SetFloat("Speed", Mathf.Abs(move));
+            // Đổi hướng nhân vật
+            if (move > 0)
+            {
+                transform.localScale = new Vector2(1, 1); // Giữ nguyên chiều cao, đổi chiều rộng thành dương
+            }
+            else if (move < 0)
+            {
+                transform.localScale = new Vector2(-1, 1); // Giữ nguyên chiều cao, đổi chiều rộng thành âm
+            }
+        }
+        else
+        {
+            // Ngừng animation chạy khi nhân vật không di chuyển trên mặt đất
+            animator.SetFloat("Speed", 0);
+        }
 
+    }
+    void Rolling()
+    {
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x * speed, rb.velocity.y);
+            //animator.SetBool("isRolling", true);
+        }
+        //else
+        //{
+        //    animator.SetBool("isRolling", false);
+        //}
+    }
     void Jump()
     {
         if ((Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow) || Input.GetKeyDown(KeyCode.Space)) && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+
+            //animator.SetBool("isJumping", true);
         }
+        //else
+        //{
+        //    // Ngừng animation nhảy khi nhân vật không nhảy
+        //    animator.SetBool("isJumping", false);
+        //}
     }
 
     void OnCollisionEnter2D(Collision2D collision)
@@ -42,6 +83,8 @@ public class Controller : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
+            //// Kích hoạt animation đứng
+            //animator.SetBool("isJumping", false);
         }
     }
 
